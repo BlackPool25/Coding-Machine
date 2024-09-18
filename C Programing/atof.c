f999289c3f2c75cefc774ca885ac0cffd2cf38c0
Shreyas S Joshi <shreyasjoshi2511@gmail.com>
2024-09-18 23:57:45 +0530

    Sync try

---
+++ C Programing/atof.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+    char ch[] = "69";
+    float num = atof(ch);
+    printf("%f", num);
+    return 0;
+}
---
+++ Python/Projects/Hand_Move/main.py
@@ -0,0 +1,22 @@
+from art import hand_1,hand_2
+import time
+import os
+import platform
+
+def cls():
+    if platform.system()=="Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def main():
+    while True:
+        print(hand_1, end=cls())
+        time.sleep(0.5)
+        print(hand_2, end=cls())
+        time.sleep(0.5)
+
+
+if __name__=="__main__":
+    main()   
