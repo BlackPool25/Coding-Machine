5787fb68f688effc7d295c98bac905d1ddaf9a9c
Shreyas S Joshi <shreyasjoshi2511@gmail.com>
2024-09-19 20:28:23 +0530

    sync

---
+++ C Programing/Practise Programs/scmin.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter the elements of the array: ");
+    for(int i = 0;i<size;i++){
+        scanf("%d", &arr[i]);
+    }
+}
--- CS50/Numb3rs/numb3rs.py
+++ CS50/Numb3rs/numb3rs.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     print(validate(input("IPv4 Address: ").strip()))
-
+ 
 
 def validate(ip):
     pattern = r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
