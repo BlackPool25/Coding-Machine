e244fae1d48c79d06561a5ba62ada85ddb49d043
Shreyas S Joshi <shreyasjoshi2511@gmail.com>
2024-09-19 21:43:01 +0530

    sync

--- C_Programing/Practise_Programs/scmin.c
+++ C_Programing/Practise_Programs/scmin.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
+int min_num(int array[], int size){
+    int min = array[0], pos = 0;
+    for(int i=0;i<size;i++){
+        if(array[i] < min){
+            min = array[i];
+            pos = i;
+        }
+    }
+    printf("%d %d", min, pos);
+}
+
+
 int main(){
     int size;
     printf("Enter the size of the array: ");
@@ -9,6 +21,5 @@ int main(){
     for(int i = 0;i<size;i++){
         scanf("%d", &arr[i]);
     }
-    for(int i=0;i<size;i++)
-    printf("%d ", arr[i]);
+    min_num(arr, size);
 }
