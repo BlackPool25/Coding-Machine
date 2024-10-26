6cb6191b29c2292ea3dfc7c264f24920577b1d66
Shreyas S Joshi <shreyasjoshi2511@gmail.com>
2024-10-26 22:38:05 +0530

    Quiz Game

---
+++ C_Programing/Multi_File_Practicse/Quiz/quiz.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int main(){
+    char questions[][100] ={"1. What year did the C language debut?: ", 
+                            "2. Who is credited with creating C?: ",
+                            "3. What is the predecessor of C?: "};
+    
+    char options[][100] = {"A. 1969", "B. 1972", "C. 1975", "D. 1999",
+                          "A. Dennis Ritchie", "B. Nikola Tesla", "C. John Carmack", "D. Doc Brown",
+                          "A. Objective C", "B. B", "C. C++", "D. C#"};
+
+    char answers[] = {'B', 'A', 'B'};
+    int numberOfQuestions = sizeof(questions)/sizeof(questions[0]);
+
+    char guess = ' ';
+    int score = 0;
+
+    printf("QUIZ GAME!\n");
+
+    for(int i = 0; i<numberOfQuestions;i++){
+        printf("*********************\n");
+        printf("%s\n", questions[i]);
+        printf("*********************\n");
+
+        for(int j=(i*4);j<(i*4)+4;j++){
+            printf("%s\n", options[j]);
+        }
+
+        printf("Guess: ");
+        scanf("%c", &guess);
+        getchar();
+
+        guess = toupper(guess);
+
+        if(guess == answers[i]){
+            printf("Correct!\n");
+            score++;
+        }
+        else{
+            printf("Wrong!");
+        }
+    }
+    printf("*********************\n");
+    printf("FINAL SCORE: %d/%d\n", score, numberOfQuestions);
+    printf("*********************\n");
+ 
+    return 0;
+
+}
--- C_Programing/Practise_Programs/test.c
+++ C_Programing/Practise_Programs/test.c
@@ -1,8 +1,13 @@
-#include<string.h>
-#include<stdio.h>
-
-int main(){
-    char rotated_t[20] = "69";
-    char i[10] = "1";
-    printf("%s", strcat(rotated_t, (char[]){i[0], '\0'}));
-}
+#include <stdio.h>
+int main() {
+int num = 10;
+char c = 'T'; //ASCII value of T is 84
+int A;
+A = num + c;
+printf("Value of A : %d\n", A);
+int  num1 = 20;
+double num2 = 50.3512897382 ;
+float B;
+B = num1 + (float)num2;
+printf("Value of B is: %f\n", B);
+return 0;} 
