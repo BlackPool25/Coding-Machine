c51078a312870eb803cca8d7563e64c2495eaf1f
Shreyas S Joshi <shreyasjoshi2511@gmail.com>
2024-11-17 21:45:55 +0530

    sync

--- CS50/Practise_Programs/classes.py
+++ CS50/Practise_Programs/classes.py
@@ -1,25 +1,47 @@
 class Student:
     def __init__(self, name, house) -> None:
-        if not name:
-            raise ValueError("Missing Name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
 
+    def __str__(self) -> str:
+        return f"{self.name} from {self.house}."
+
+
+    @property
+    def name(self):
+        return self._name
+
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing Name")
+        self._name = name
+
+
+    @property
+    def house(self):
+        return self._house
+
+
+    @house.setter
+    def house(self,house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 
 def get_student():
-    name = input("Name: ").title()
-    house = input("House: ").title()
-    # try:
-    return Student(name,house)
-    # except ValueError:
-    
+    name = input("Name: ").strip().title()
+    house = input("House: ").strip().title()
+    return Student(name, house)
+
 
 if __name__ == "__main__":
     main()
