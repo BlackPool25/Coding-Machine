6de05fa8105bd6c044ff85e2dc71463d56d13fdd
Shreyas S Joshi <shreyasjoshi2511@gmail.com>
2024-11-18 11:17:44 +0530

    sync

---
+++ CS50/Practise_Programs/hat.py
@@ -0,0 +1,13 @@
+class Hat:
+    def sort(self, name):
+        print(name, "is in", "some house.")
+
+
+def main():
+    hat = Hat()
+    hat.sort("Harry")
+
+
+
+if __name__ == "__main__":
+    main()
