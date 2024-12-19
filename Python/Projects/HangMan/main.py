28088fb11e8fda659072e0e2cff948e14cf31f8a
Shreyas S Joshi <shreyasjoshi2511@gmail.com>
2024-12-19 08:07:38 +0530

    sync

--- Python/Projects/HangMan/main.py
+++ Python/Projects/HangMan/main.py
@@ -12,66 +12,91 @@ def clear():
     else:
         os.system('clear')
 
-clear()
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
-
-end_of_game = False
-lives = 6
-
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-from hangman_art import logo, stages
-print(logo)
-#Testing code
-#print(f'Pssst, the solution is {chosen_word}.')
-
-#Create blanks
-display = []
-for _ in range(word_length):
-    print("_ ", end = "")
-    display += "_"
-
-print()
-    
-
-
-while not end_of_game:
-    
-    guess = input("Guess a letter: ").lower()
+def game():
+    setofguess = set()
     clear()
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    not_in_word = []
+    chosen_word = random.choice(word_list)
+    word_length = len(chosen_word)
 
-    #Check guessed letter
-    if guess in display:
-        print(f"You have already guessed {guess}")
+    end_of_game = False
+    lives = 6
 
-    else:
-        for position in range(word_length):
-            letter = chosen_word[position]
-            if letter == guess:
-                display[position] = letter
-    
+    #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+    from hangman_art import logo, stages
+    print(logo)
+    #Testing code
+    #print(f'Pssst, the solution is {chosen_word}.')
+
+    #Create blanks
+    display = []
+    for _ in range(word_length):
+        print("_ ", end = "")
+        display += "_"
+
+    print()
+        
+
+
+    while not end_of_game:
+        print(setofguess)
+        guess = input("Guess a letter: ").lower()
+        
+        if guess in setofguess:
+            continue
+        else:
+            setofguess.add(guess)
+        clear()
+        #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
+        #Check guessed letter
+        if guess in display:
+            print(f"You have already guessed {guess}")
+
+        else:
+            for position in range(word_length):
+                letter = chosen_word[position]
+                if letter == guess:
+                    display[position] = letter
         
             
-    #Check if user is wrong.
-    
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-        if guess not in chosen_word:
-            lives -= 1
-            print(f"{guess} is not in the word. You lose a life.")
-        if lives == 0:
+                
+        #Check if user is wrong.
+        
+            
+            if guess not in chosen_word:
+                lives -= 1
+                print(f"{guess} is not in the word. You lose a life.")
+            if lives == 0:
+                end_of_game = True
+                print("You lose.")
+                print(f"The word was {chosen_word}")
+
+        #Join all the elements in the list and turn it into a String.
+        print(f"{' '.join(display)}")
+
+        #Check if user has got all letters.
+        if "_" not in display:
             end_of_game = True
-            print("You lose.")
-            print(f"The word was {chosen_word}")
+            print("You win.")
 
-    #Join all the elements in the list and turn it into a String.
-    print(f"{' '.join(display)}")
+        #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+        print(stages[lives])
+    
 
-    #Check if user has got all letters.
-    if "_" not in display:
-        end_of_game = True
-        print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
-    print(stages[lives])
+
+def main():
+    while True:
+        word = game()
+        if input("Do you wanna try again:").strip().lower() == "y":
+            continue
+        break
+
+
+
+
+
+if __name__ == "__main__":
+    main()
     
